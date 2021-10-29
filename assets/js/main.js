3414c7c459ebd0196bf92637e2c9151bfade72af
MaxBePunkt <maximillianborret@gmail.com>
2021-10-29 14:13:41 +0200

    1.2

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// console.log("test");
+
+function subEmail() {
+  let email = document.getElementById("email").value;
+
+  console.log(email);
+}
+
+function viewPop() {
+  var popup = document.getElementById("popUp");
+
+  console.log(popup);
+  popup.classList.toggle("show");
+}
